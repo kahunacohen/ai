a501c2c2c53fb98c5b627b131131e0c84174b9ca
Aaron Cohen <aaroncohendev@gmail.com>
2023-04-20 19:47:59 +0300

    add validating script

---
+++ validate_raw_sql.py
@@ -0,0 +1,25 @@
+"""Parse queries.sql, extract base line queries and run them. This validates they are well-formed etc."""
+import csv
+import sqlite3
+import sys
+
+from yachalk import chalk
+
+
+con = sqlite3.connect("northwind.db")
+cur = con.cursor()
+try:
+    with open('./queries.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter="|")
+        for row in list(reader)[1:]:
+            sql = row[1]
+            print(f'Querying: {sql}')
+            res = cur.execute(sql).fetchall()
+            for row in res:
+                print(chalk.gray("".join(row)))
+            print(chalk.green("success!"))
+except Exception as err:
+    print(chalk.red(f'Problem running query: {err}'))
+    sys.exit(1)
+finally:
+    con.close()
