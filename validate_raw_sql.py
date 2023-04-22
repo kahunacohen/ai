f1b4d58cd0fa071d63506b1e12d6579114c00566
Aaron Cohen <aaroncohendev@gmail.com>
2023-04-23 00:00:11 +0300

    add order date select

--- validate_raw_sql.py
+++ validate_raw_sql.py
@@ -10,13 +10,13 @@ con = sqlite3.connect("northwind.db")
 cur = con.cursor()
 try:
     with open('./queries.csv', newline='') as csvfile:
-        reader = csv.reader(csvfile, delimiter="|")
+        reader = csv.reader(csvfile, delimiter=":")
         for row in list(reader)[1:]:
             sql = row[1]
             print(f'Querying: {sql}')
             res = cur.execute(sql).fetchall()
             for row in res:
-                print(chalk.gray("".join(row)))
+                print(chalk.gray("".join([str(col) for col in row])))
             print(chalk.green("success!"))
 except Exception as err:
     print(chalk.red(f'Problem running query: {err}'))
