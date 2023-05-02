0a82e00381ed11e4fdf395d00f27a62159d044bc
Aaron Cohen <aaroncohendev@gmail.com>
2023-05-02 14:55:24 +0300

    add script

--- fine_tune.py
+++ fine_tune.py
@@ -1,5 +1,11 @@
+'''This script produces the jsonl from the csv file. After this is run,
+$ openai tools fine_tunes.prepare_data -f queries.jsonl'''
+from yachalk import chalk
 import pandas as pd
-print("hi")
 
-data = pd.read_csv("./queries.csv", header=None, delimiter=":")
-print(data)
+
+data = pd.read_csv("./queries.csv", delimiter=":").drop(columns=['training'])
+print("\nFirst 10 lines of data parsed from queries.csv:")
+print(chalk.gray(data.head()))
+data.to_json("queries.jsonl",orient='records',lines=True)
+print(chalk.green("Succesfully translated .csv files to queries.jsonl file."))
